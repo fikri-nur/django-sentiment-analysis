4bb26022330aa276913d91f9acc7db68a9f7665f
fikri-nur <fikriiinur07@gmail.com>
2024-06-13 21:43:13 +0700

    Add detail and delete fitur for evaluations

--- sentiment_analysis_nb_svm/evaluasi/urls.py
+++ sentiment_analysis_nb_svm/evaluasi/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.indexView, name='index_view'),
+    path('detail/<int:id>', views.detailView, name='detail_view'),
+    path('delete/<int:id>', views.deleteView, name='delete_view')
 ]
--- sentiment_analysis_nb_svm/evaluasi/views.py
+++ sentiment_analysis_nb_svm/evaluasi/views.py
@@ -1,7 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Evaluation
+import os
+import shutil
 # Create your views here.
 
 def indexView(request):
     evalDatas = Evaluation.objects.all()
+    
+    for eval in evalDatas:
+        eval.test_size = round(eval.test_size, 1)
+        eval.train_size = round(eval.train_size, 1)
+        eval.accuracy = round(eval.accuracy, 2)
+        eval.precision = round(eval.precision, 2)
+        eval.recall = round(eval.recall, 2)
+        eval.f1_score = round(eval.f1_score, 2)
+        
+        eval.test_size = f"{eval.test_size * 100}%"
+        eval.train_size = f"{eval.train_size * 100}%"
+        eval.accuracy = f"{eval.accuracy * 100}%"
+        eval.precision = f"{eval.precision * 100}%"
+        eval.recall = f"{eval.recall * 100}%"
+        eval.f1_score = f"{eval.f1_score * 100}%"
+        
     return render(request, 'evaluasi/index.html', {'evaluations': evalDatas})
+
+def detailView(request, id):
+    evalData = Evaluation.objects.get(id=id)
+    evalData.test_size = round(evalData.test_size, 1)
+    evalData.train_size = round(evalData.train_size, 1)
+    evalData.accuracy = round(evalData.accuracy, 2)
+    evalData.precision = round(evalData.precision, 2)
+    evalData.recall = round(evalData.recall, 2)
+    evalData.f1_score = round(evalData.f1_score, 2)
+    
+    evalData.test_size = f"{evalData.test_size * 100}%"
+    evalData.train_size = f"{evalData.train_size * 100}%"
+    evalData.accuracy = f"{evalData.accuracy * 100}%"
+    evalData.precision = f"{evalData.precision * 100}%"
+    evalData.recall = f"{evalData.recall * 100}%"
+    evalData.f1_score = f"{evalData.f1_score * 100}%"
+    
+    
+    evalData.confusion_matrix_path = evalData.confusion_matrix_path.replace("D:\\Kuliah\\Semester 8\\Sistem\\python\\sentiment_analysis_nb_svm\\pemodelan\\static", "")
+    
+    return render(request, 'evaluasi/detail.html', {'evaluation': evalData})
+
+def deleteView(request, id):
+    evalData = Evaluation.objects.get(id=id)
+    folder = evalData.confusion_matrix_path.replace("D:\\Kuliah\\Semester 8\\Sistem\\python\\sentiment_analysis_nb_svm\\pemodelan\\", "pemodelan/").replace("\\confusion_matrix.png", "")
+    
+    # Delete the folder and its content
+    try:
+        shutil.rmtree(folder)
+    except Exception as e:
+        print(f'Failed to delete {folder}. Reason: {e}')
+    evalData.delete()
+    return redirect('evaluasi:index_view')
